495b4c0b4d6c1a733534df9d185d238339a86e60
ZamPaul <zamielpaul2@gmail.com>
2025-10-24 15:30:02 +0500

    new weekly practice

---
+++ lab-report/lab_report_week_5.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+
+void verifyVoter(){
+    int age = -50;
+    char selection = 'Z';
+
+    while (age < 0) {
+        cout << "Please enter an age greater than 0: ";
+        cin >> age;
+    }
+
+    if (age >= 18) {
+        while (!(selection == 'y' || selection == 'Y' || selection == 'n' || selection == 'N')) {
+            cout << "Do you have a voter ID (Enter Y/N): ";
+            cin >> selection;
+        }
+
+        if (selection == 'y' || selection == 'Y') {
+            cout << "Congrats! You are Eligible for voting!" << endl;
+        } else {
+            cout << "Sorry! You are not Eligible for voting (no voter ID)!" << endl;
+        }
+    } else {
+        cout << "Sorry! You are too young for voting!" << endl;
+    }
+}
+
+void checkEvenOddNumber(){
+    int num;
+
+    cout << "Enter a number to check: ";
+    cin >> num;
+
+    while (num <= 0){
+        cout << "Enter a number to check: ";
+        cin >> num;
+    }
+
+    if(num % 2 == 0){
+        // number is even
+        cout << "Number is even";
+
+        if(num % 4 == 0){
+            cout << " and also divisible by four";
+        }else{
+            cout << ", but not divisible by four";
+        }
+    }else {
+        cout << "Number is odd";
+        
+        if(num % 3 == 0){
+            cout << " and also divisible by three";
+        }else{
+            cout << ", but not divisible by three";
+        }
+    }
+}
+
+int main() {
+    verifyVoter();
+
+    checkEvenOddNumber();
+
+    return 0;
+}
---
+++ weekly-practice/FunctionChallengeTasks.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+// challenge 1:
+// write a function that returns true in a number is prime otherwise false
+int isPrime(){
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if(num <= 1){
+        // 0 and 1 are not prime number;
+        return false;
+    }
+
+    // looping from 2 to half of the original number
+    for(int i = 2; i <= num / 2; i++){
+        if(num % i == 0){
+            return false; 
+        }
+    }
+
+    // no divisible numbers found
+    return true;
+}
+
+int convertToFarenheit(float temp){
+    float farenheitValue = (temp * 9/5) + 32;
+    
+    return farenheitValue;
+}
+
+int main(){
+    // prime number code here =>
+    bool result = isPrime();
+
+    if(result){
+        cout << "Number is Prime" << endl;
+    }else{
+        cout << "Number is not Prime" << endl;
+    }
+
+
+    // celcius coversion code here =>
+    float celciusValue;
+    float newValue;
+    cout << "Enter temperature to convert: ";
+    cin >> celciusValue;
+
+    newValue = convertToFarenheit(celciusValue);
+
+    cout << "Temparature in Celcius: " << celciusValue << endl;
+    cout << "Temparature in Farenheit: " << newValue << endl;
+
+    return 0;
+}
